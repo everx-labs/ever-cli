fd9fccf80f36d1e03acd7da6b025a027b99c8c5a
Nikita.m <nikita.m@tonlabs.io>
2020-07-02 12:54:43 +0300

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARR{ANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific TON DEV software governing permissions and
  * limitations under the License.
  */
