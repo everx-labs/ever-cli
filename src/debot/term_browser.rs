969fa6e359c513116007a55ff921f8e8297a19cf
Nikita.m <nikita.m@tonlabs.io>
2021-03-05 18:09:31 +0300

    Use default trait

--- src/debot/term_browser.rs
+++ src/debot/term_browser.rs
@@ -105,12 +105,9 @@ impl TerminalBrowser {
                 ParamsOfEncodeInternalMessage {
                     abi: Some(debot.abi.clone()),
                     address: Some(debot_addr.to_owned()),
-                    src_address: None,
-                    deploy_set: None,
                     call_set,
                     value: "1000000000000000".to_owned(),
-                    bounce: None,
-                    enable_ihr: None,
+                    ..Default::default()
                 }
             )
             .await
