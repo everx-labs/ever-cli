c409c6e2353d3346bf1b87e7a26b07458596168e
SilkovAlexander <slexander.s@tonlabs.io>
2021-09-21 21:22:21 +0300

    Fixed test for not to work with net ton dev

--- tests/cli.rs
+++ tests/cli.rs
@@ -654,7 +654,7 @@ fn test_override_config_path() -> Result<(), Box<dyn std::error::Error>> {
 fn test_sendfile() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(BIN_NAME)?;
     cmd.arg("--url")
-        .arg("https://net.ton.dev")
+        .arg(&*NETWORK)
         .arg("message")
         .arg("0:841288ed3b55d9cdafa806807f02a0ae0c169aa5edfe88a789a6482429756a94")
         .arg("sendTransaction")
@@ -676,7 +676,7 @@ fn test_sendfile() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut cmd = Command::cargo_bin(BIN_NAME)?;
     cmd.arg("--url")
-        .arg("https://net.ton.dev")
+        .arg(&*NETWORK)
         .arg("sendfile")
         .arg("call.boc");
     cmd.assert()
