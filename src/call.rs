a2fc592e2b14fdfa006e5c14f02d274995f6ffb8
SilkovAlexander <slexander.s@tonlabs.io>
2021-10-05 14:18:10 +0300

    Added json output for runget command

--- src/call.rs
+++ src/call.rs
@@ -55,7 +55,7 @@ use ton_client::tvm::{
 };
 use ton_block::{Account, Serializable, Deserializable};
 use std::str::FromStr;
-use serde_json::Value;
+use serde_json::{Value, Map};
 
 pub struct EncodedMessage {
     pub message_id: String,
@@ -715,7 +715,9 @@ pub async fn run_get_method(conf: Config, addr: &str, method: &str, params: Opti
         .transpose()
         .map_err(|e| format!("arguments are not in json format: {}", e))?;
 
-    println!("Running get-method...");
+    if !conf.is_json {
+        println!("Running get-method...");
+    }
     let result = run_get(
         ton,
         ParamsOfRunGet {
@@ -728,7 +730,25 @@ pub async fn run_get_method(conf: Config, addr: &str, method: &str, params: Opti
     .map_err(|e| format!("run failed: {}", e.to_string()))?
     .output;
 
-    println!("Succeeded.");
-    println!("Result: {}", result);
+    if !conf.is_json {
+        println!("Succeeded.");
+        println!("Result: {}", result);
+    } else {
+        let mut res = Map::new();
+        match result {
+            Value::Array(array) => {
+                let mut i = 0;
+                for val in array.iter() {
+                    res.insert(format!("value{}", i), val.to_owned());
+                    i = 1 + i;
+                }
+            },
+            _ => {
+                res.insert("value0".to_owned(), result);
+            }
+        }
+        let res = Value::Object(res);
+        println!("{}", serde_json::to_string_pretty(&res).unwrap_or("Undefined".to_string()));
+    }
     Ok(())
 }
