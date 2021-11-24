8688c23c6b51c1aafe2f236bc33d4dc86cfc99fa
Amphyby <amphyby@protonmail.com>
2021-11-24 11:48:59 +0300

    with 5 new tests for separate interfaces

---
+++ tests/samples/ConfirmInput.sol
@@ -0,0 +1,39 @@
+pragma ton-solidity >=0.35.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+import "Debot.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Terminal/Terminal.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/ConfirmInput/ConfirmInput.sol";
+
+contract ConfirmInputDebot is Debot {
+    function start() public override {
+       ConfirmInput.get(tvm.functionId(setResult), "Select:");
+    }
+
+    function setResult(bool value) public {
+        if (value) {
+            Terminal.print(0, "ConfirmInput tests completed!");
+        }
+    }
+    
+    function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
+        return [ Terminal.ID, ConfirmInput.ID ];
+    }
+
+    function getDebotInfo() public functionID(0xDEB) override view returns(
+        string name, string version, string publisher, string caption, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "ConfirmInput example DeBot";
+        version = "0.1.0";
+        publisher = "TON Labs";
+        caption = "How to use the ConfirmInput interface";
+        author = "TON Labs";
+        support = address(0);
+        hello = "Hello, i am an ConfirmInput example DeBot.";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = "";
+    }
+}
