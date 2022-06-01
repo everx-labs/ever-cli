0e7085282c4f7edef95835da39661d8f71830b9f
SilkovAlexander <slexander.s@tonlabs.io>
2022-06-01 18:10:36 +0300

    Added little fix

--- src/main.rs
+++ src/main.rs
@@ -913,10 +913,8 @@ async fn main_internal() -> Result <(), String> {
                 if !is_json {
                     format!("Error: {}", e)
                 } else {
-                    let err: serde_json::Value = match serde_json::from_str(&e) {
-                        Ok(res) => {res},
-                        Err(_) => { serde_json::Value::String(e)}
-                    };
+                    let err: serde_json::Value = serde_json::from_str(&e)
+                        .unwrap_or(serde_json::Value::String(e));
                     let res = json!({"Error": err});
                     serde_json::to_string_pretty(&res)
                         .unwrap_or("{{ \"JSON serialization error\" }}".to_string())
